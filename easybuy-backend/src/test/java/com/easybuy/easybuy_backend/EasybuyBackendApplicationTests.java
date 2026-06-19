f52d369c759cb7495ff39b752150d9925c57e204
Khushwant-Kumar-Suthar <khushwantsuthar49@gmail.com>
2026-06-19 23:16:31 +0530

    Initialize Spring Boot backend project structure

---
+++ easybuy-backend/src/test/java/com/easybuy/easybuy_backend/EasybuyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybuy.easybuy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybuyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
