23599605a8e63b5e77c8e2e33e6a4363b113df8b
Khushwant-Kumar-Suthar <khushwantsuthar49@gmail.com>
2026-06-19 22:10:40 +0530

    01 Product Details page design

---
+++ easybuy-frontend/src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './components/Navigation/Navigation'
+import HeroSection from './components/HeroSection/HeroSection'
+import NewArrivals from './components/Sections/NewArrivals'
+const Shop =()=>{
+    <>
+    <Navigation />
+    <HeroSection />
+    <NewArrivals />
+    
+    </>
+}
+
+export default Shop
