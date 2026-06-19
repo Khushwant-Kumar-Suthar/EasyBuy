3b1b931d2a33b2f46bc40e7b42e93c6f09ad26a5
Khushwant-Kumar-Suthar <khushwantsuthar49@gmail.com>
2026-06-19 11:58:14 +0530

    Login and other pages create

--- easybuy-frontend/src/index.js
+++ easybuy-frontend/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Shop from './Shop';
-import reportWebVitals from './reportWebVitals';
-
+import reportWebVitals from './reportWebVitals'
+import 'react-multi-carousel/lib/styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
